8609e0dea261d6dc3f5249fb9a3e053b644bd1cf
Lucas Paes <lpjanuzi@gmail.com>
2022-10-03 22:16:13 -0300

    disponibilizando no github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { installViewer } from '@ohif/viewer';
+//import {OHIFExtDicomMicroscopy} from '@ohif/extension-dicom-microscopy'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    window.config = {
+      routerBasename: '/',
+      filterQueryParam: true,
+//      extensions: [OHIFExtDicomMicroscopy],
+      servers: {
+        dicomWeb: [
+          {
+            name: 'DCM4CHEE',
+            wadoUriRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/wado',
+            qidoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
+            wadoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
+            qidoSupportsIncludeField: true,
+            imageRendering: 'wadors',
+            thumbnailRendering: 'wadors',            
+            // enableStudyLazyLoad: true,
+            // studyInstanceUIDs: this.props.studyID,
+          }
+        ]
+      } 
+    }; 
+  
+
+  this.ohifViewerConfig = window.config; // or set it here
+  this.containerId = 'ohif';
+}
+
+
+  componentRenderedOrUpdatedCallback = function() {
+      console.log('OHIF Viewer rendered/updated');
+  };
+
+  componentDidMount() {
+    installViewer(
+        this.ohifViewerConfig,
+        this.containerId,
+        this.componentRenderedOrUpdatedCallback
+      );
+  }
+
+  render () { 
+    return(
+      <div id={this.containerId}/>
+    )
+  }
 }
 
 export default App;
